9c84cf6247677b333ebdb07cc6ae28b878e794c3
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-14 21:27:40 -0800

    added Header and Resources components, actions and reducer

---
+++ src/components/Resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Resources = () => {
+  return(
+    <div>
+      Ingredients
+      <ul>
+        <li>Sugar</li>
+        <li>Flour</li>
+        <li>Salt</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Resources;
