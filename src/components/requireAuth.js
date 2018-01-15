a4d66b1d83798a2ecd0580e22e6ed2ce586e70a8
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-14 22:18:38 -0800

    protected /resources route to require sign-in

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -3,6 +3,22 @@ import { connect } from 'react-redux';
 
 export default function(ComposedCompnent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedCompnent {...this.props} />
     }
